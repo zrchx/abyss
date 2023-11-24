ba9e1bdf2b2857b3c6fa9347c975a2c4640af629
zrchx <epsilonrf0@gmail.com>
2023-11-24 01:34:51 -0500

    first attemp

---
+++ lua/abyss/palette.lua
@@ -0,0 +1,18 @@
+local palette = {
+  -- Base
+  bg = "#1A1A1A",
+  bg_alt = "#2A2A2A",
+  fg_alt = "#3B3B3B",
+  fg = "#6B6B6B",
+  -- Colors
+  red = "#eaaeae",
+  orange = "#7fa2bd",
+  green = "#67879e",
+  blue = "#a3c5e0",
+  magenta = "#a3c5e0",
+  cyan = "#67879e",
+  yellow = "#7fa2bd",
+
+  none = "NONE",
+}
+return palette
