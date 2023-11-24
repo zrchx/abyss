321dbf592cf057d8ba87b5f573d9af61e4c51e7a
zrchx <epsilonrf0@gmail.com>
2023-11-24 01:54:49 -0500

    lualine

--- lua/lualine/themes/abyss.lua
+++ lua/lualine/themes/abyss.lua
@@ -4,31 +4,31 @@ return {
     normal = {
         a = { bg = p.bg_alt, fg = p.cyan },
         b = { bg = p.bg_alt, fg = p.fg },
-        c = { bg = p.bg_alt, fg = p.fg_alt },
+        c = { bg = p.bg_alt, fg = p.fg },
     },
     insert = {
         a = { bg = p.bg_alt, fg = p.red },
         b = { bg = p.bg_alt, fg = p.fg },
-        c = { bg = p.bg_alt, fg = p.fg_alt },
+        c = { bg = p.bg_alt, fg = p.fg },
     },
     command = {
         a = { bg = p.bg_alt, fg = p.blue },
         b = { bg = p.bg_alt, fg = p.fg },
-        c = { bg = p.bg_alt, fg = p.fg_alt },
+        c = { bg = p.bg_alt, fg = p.fg },
     },
     visual = {
         a = { bg = p.bg_alt, fg = p.yellow },
         b = { bg = p.bg_alt, fg = p.fg },
-        c = { bg = p.bg_alt, fg = p.fg_alt },
+        c = { bg = p.bg_alt, fg = p.fg },
     },
     replace = {
         a = { bg = p.bg_alt, fg = p.magenta },
         b = { bg = p.bg_alt, fg = p.fg },
-        c = { bg = p.bg_alt, fg = p.fg_alt },
+        c = { bg = p.bg_alt, fg = p.fg },
     },
     inactive = {
-        a = { bg = p.bg_alt, fg = p.fg_alt },
-        b = { bg = p.bg_alt, fg = p.fg_alt },
-        c = { bg = p.bg_alt, fg = p.fg_alt },
+        a = { bg = p.bg_alt, fg = p.fg },
+        b = { bg = p.bg_alt, fg = p.fg },
+        c = { bg = p.bg_alt, fg = p.fg },
     },
 }
